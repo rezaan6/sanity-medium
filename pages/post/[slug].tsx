f27b3d251a4737e98555eb44bdad661472fce214
Rezaan Riyaz <rezaan.riyaz@kodez.com.au>
2022-09-10 20:03:41 +0530

    Update [slug].tsx

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -46,15 +46,15 @@ function Post({ post }: Props) {
  
     <main>
       <Header />
-      <img className="w-full h-40 object-cover" src={urlFor(post.mainImage).url()!} alt="" />
+      <img className="object-cover w-full h-40" src={urlFor(post.mainImage).url()!} alt="" />
 
-      <article className="max-w-3xl mx-auto p-5">
-        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
-        <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+      <article className="max-w-3xl p-5 mx-auto">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">{post.description}</h2>
 
         <div className="flex items-center space-x-2">
-          <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
-          <div className="font-extralight text-sm ">
+          <img className="w-10 h-10 rounded-full" src={urlFor(post.author.image).url()!} alt="" />
+          <div className="text-sm font-extralight ">
             Blog post by <span className="text-green-600">{post.author.name}</span> - Published at{" "}
             {""}
             {post._createdAt}
@@ -68,8 +68,8 @@ function Post({ post }: Props) {
             projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
             content={post.body}
             serializers={{
-              h1: (props: any) => <h1 className="text-2xl font-bold my-5" {...props} />,
-              h2: (props: any) => <h1 className="text-xl font-bold my-5" {...props} />,
+              h1: (props: any) => <h1 className="my-5 text-2xl font-bold" {...props} />,
+              h2: (props: any) => <h1 className="my-5 text-xl font-bold" {...props} />,
               li: ({ children }: any) => <li className="ml-4 list-disc">{children} </li>,
               link: ({ href, children }: any) => (
                 <a href={href} className="text-blue-500 hover:underline">
@@ -81,17 +81,17 @@ function Post({ post }: Props) {
         </div>
       </article>
 
-      <hr className="max-w-lg  my-5 mx-auto border border-yellow-500" />
+      <hr className="max-w-lg mx-auto my-5 border border-yellow-500" />
 
       {submitted ? (
-        <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+        <div className="flex flex-col max-w-2xl p-10 mx-auto my-10 text-white bg-yellow-500">
           <h3 className="text-3xl font-bold">Thank you for Submitting</h3>
           <div>Once it has been approved , it will appear below!</div>
         </div>
       ) : (
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col p-5 my-10 max-w-2xl mx-auto mb-10 "
+          className="flex flex-col max-w-2xl p-5 mx-auto my-10 mb-10 "
         >
           <h3 className="text-sm text-yellow-500">Enjoy this article?</h3>
           <h4 className="text-3xl font-bold">Leave a comment below!</h4>
@@ -103,7 +103,7 @@ function Post({ post }: Props) {
             <span className="text-gray-700">Name</span>
             <input
               {...register("name", { required: true })}
-              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring "
+              className="block w-full px-3 py-2 mt-1 border rounded shadow outline-none form-input ring-yellow-500 focus:ring "
               placeholder="John"
               type="text"
             />
@@ -113,7 +113,7 @@ function Post({ post }: Props) {
             <span className="text-gray-700">Email</span>
             <input
               {...register("email", { required: true })}
-              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring "
+              className="block w-full px-3 py-2 mt-1 border rounded shadow outline-none form-input ring-yellow-500 focus:ring "
               placeholder="John"
               type="email"
             />
@@ -123,7 +123,7 @@ function Post({ post }: Props) {
             <span className="text-gray-700">Comment</span>
             <textarea
               {...register("comment", { required: true })}
-              className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              className="block w-full px-3 py-2 mt-1 border rounded shadow outline-none form-textarea ring-yellow-500 focus:ring"
               placeholder="John"
               rows={8}
             />
@@ -140,14 +140,14 @@ function Post({ post }: Props) {
 
           <input
             type="submit"
-            className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
+            className="px-4 py-2 font-bold text-white bg-yellow-500 rounded shadow cursor-pointer hover:bg-yellow-400 focus:shadow-outline focus:outline-none"
           />
         </form>
       )}
 
       {/* Comments */}
     
-      <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+      <div className="flex flex-col max-w-2xl p-10 mx-auto my-10 space-y-2 shadow shadow-yellow-500">
    <>
         <h3 className="text-4xl">Comments</h3>
         <hr className="pb-2" />
@@ -205,7 +205,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       },
       'comments': *[
         _type == "comment" && 
-        post.ref == ^.id],
+        post.ref == ^.id &&
+      approved == true],
       description,
       mainImage,
       slug,
